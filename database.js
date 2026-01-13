9f0a60764006e4a1b0619949fa65aedb27886e98
Diego <davinodiego2@gmail.com>
2026-01-13 02:25:28 +0100

    Delete database.js

--- database.js
+++
@@ -1,4 +0,0 @@
-const databaseEdile=[
-{Categoria:"MANODOPERA",Sottocategoria:"Base",Descrizione:"Manodopera",Unità_Misura:"h",Prezzo_Unitario:"35"},
-{Categoria:"MATERIALI",Sottocategoria:"Vari",Descrizione:"Materiali",Unità_Misura:"cad",Prezzo_Unitario:"10"}
-];
